90d37de83a984967fe4d5e6f2231c559d0f81896
Marius Povilonis <povilonismarius@gmail.com>
2024-04-29 23:51:11 +0300

    exchange rates

--- app/src/main/java/com/example/fintech_3/MainActivity.kt
+++ app/src/main/java/com/example/fintech_3/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.fintech_3
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ListView
 import android.widget.SimpleAdapter
@@ -14,6 +16,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.lang.reflect.Type
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
         listView = findViewById(R.id.listView)
         updateList()
+
     }
 
     fun onCreateClick(view: View) {
@@ -69,5 +75,44 @@ class MainActivity : AppCompatActivity() {
         )
 
         listView.adapter = adapter
+    }
+
+    fun onApiRequestClick(view:View){
+        Executors.newSingleThreadExecutor().execute {
+            val url = URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
+
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "GET"  // optional default is GET
+
+                println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+                inputStream.bufferedReader().use {
+                    val pref = applicationContext.getSharedPreferences(getString(R.string.notes_key), Context.MODE_PRIVATE)
+                    val editor = pref.edit()
+                    val gson = Gson()
+
+                    val oldJson = pref.getString(getString(R.string.notes_key), "[]")
+                    val type: Type = object : TypeToken<List<Note>>() {}.type
+                    val arrayList: ArrayList<Note> = gson.fromJson(oldJson, type) as ArrayList<Note>
+
+                    it.lines().forEach { line ->
+                        println(line)
+                        if (line.contains("<Cube currency")) {
+                            val words: List<String> = line.split("'")
+                            val note: Note = Note(name = words[1], description = words[3])
+
+                            arrayList.add(note)
+                        }
+                    }
+
+                    val newJson = gson.toJson(arrayList)
+                    editor.putString(getString(R.string.notes_key), newJson)
+                    editor.commit()
+                    Handler(Looper.getMainLooper()).post {
+                        updateList()
+                    }
+                }
+            }
+        }
     }
 }
